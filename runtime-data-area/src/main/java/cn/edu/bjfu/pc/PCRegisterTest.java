744fa3be10eee4b1251ba97211d8b1011b935ce6
CHAOS2614 <37375653+CHAOS2614@users.noreply.github.com>
2021-11-26 22:05:47 +0800

    程序计数器--Program Counter Register

---
+++ runtime-data-area/src/main/java/cn/edu/bjfu/pc/PCRegisterTest.java
@@ -0,0 +1,17 @@
+package cn.edu.bjfu.pc;
+
+/**
+ * @author chaos
+ * @date 2021-11-26 21:22
+ */
+public class PCRegisterTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+}
