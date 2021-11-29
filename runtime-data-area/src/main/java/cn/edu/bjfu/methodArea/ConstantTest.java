799d4f77c1f7de07dedd9d8682e56484512a88ea
CHAOS2614 <37375653+CHAOS2614@users.noreply.github.com>
2021-11-29 14:59:56 +0800

    1129-方法区

---
+++ runtime-data-area/src/main/java/cn/edu/bjfu/methodArea/ConstantTest.java
@@ -0,0 +1,14 @@
+package cn.edu.bjfu.methodArea;
+
+/**
+ * @author chaos
+ * @date 2021-11-29 10:50
+ */
+public class ConstantTest {
+    private int n = 10;
+
+    public void test(){
+        int j = n + 1;
+        System.out.println("test");
+    }
+}
