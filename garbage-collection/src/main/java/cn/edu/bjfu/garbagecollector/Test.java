007ddace21bc45c7dc4b26a7ace833784d738f80
CHAOS2614 <37375653+CHAOS2614@users.noreply.github.com>
2021-12-01 00:21:27 +0800

    1130-垃圾回收三种基本算法

---
+++ garbage-collection/src/main/java/cn/edu/bjfu/garbagecollector/Test.java
@@ -0,0 +1,34 @@
+package cn.edu.bjfu.garbagecollector;
+
+/**
+ * @author chaos
+ * @date 2021-11-30 21:25
+ */
+public class Test {
+
+    public static Test obj;
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        obj = this;
+    }
+
+    public static void main(String[] args) {
+        obj = new Test();
+        System.out.println(obj);
+        obj = null;
+        System.gc();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (obj == null) {
+            System.out.println("obj d");
+        } else {
+            System.out.println(obj);
+        }
+    }
+
+}
